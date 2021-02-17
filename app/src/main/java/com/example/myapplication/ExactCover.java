05107a21daf53797af2118246c777c25838d5a57
tgorif <kay.st.janssen@googlemail.com>
2021-02-18 00:26:09 +0100

    st

---
+++ app/src/main/java/com/example/myapplication/ExactCover.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+public class ExactCover implements IStrategySolve{
+    public boolean solve(int[][] grid) {
+        return false;
+    }
+    public void printAnalytics() {
+
+    }
+    public void printSolution() {
+
+    }
+}
